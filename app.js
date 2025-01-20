9ba224a2717c5dc369036b34a92969468e4a4224
Carlovich01 <carlosemanuelok2@gmail.com>
2025-01-20 04:15:22 -0300

    Funcion Sortear Amigo

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear un array para almacenar los nombres
 let listaAmigos = [];
-let nombreEscrito = "";
-let listaHtml = "";
 
 function agregarAmigo() {
-    nombreEscrito = document.getElementById("campoNombre").value;
+    //Capturar el valor del campo de entrada
+    let nombreEscrito = document.getElementById("campoNombre").value;
+    //Validar la entrada
     if (nombreEscrito === "") {
         alert("Por favor, inserte un nombre.");
     }
     else {
+        //Actualizar el array de amigos
         listaAmigos.push(nombreEscrito);
+        //Limpiar el campo de entrada
         document.getElementById("campoNombre").value = [];
+        //Actualizar lista en pantalla
         actualizarLista();
     }
 }
 
 function actualizarLista() {
-    listaHtml = document.getElementById("listaAmigos");
+    //Obtener el elemento de la lista
+    let listaHtml = document.getElementById("listaAmigos");
+    //Limpiar la lista existente
     listaHtml.innerHTML = "";
+    //Iterar sobre el arreglo
     for(let i=0; i<listaAmigos.length; i++) {
+        //Agregar elementos a la lista
         listaHtml.innerHTML += `<li> ${listaAmigos[i]} </li>`;
     }
 }
 
+function sortearAmigo() {
+    //Validar que haya amigos disponibles:
+    if(listaAmigos.length === 0) {
+        alert("No hay amigos para sortear.");
+    }
+    else{
+        //Limpiar la lista existente
+        let listaHtml = document.getElementById("listaAmigos");
+        listaHtml.innerHTML = "";
+        //Generar un indice aleatorio y obtener el nombre sorteado
+        let amigoSorteado = listaAmigos[parseInt(Math.random()*listaAmigos.length)];
+        //Mostrar el resultado
+        let mostrarSorteo = document.getElementById("resultado");
+        mostrarSorteo.innerHTML = "";
+        mostrarSorteo.innerHTML = `<h3>El amigo sorteado es: ${amigoSorteado}</h3>`;
+    }
+}
